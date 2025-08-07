122d91b1c00cd83446ab1866c6b82d3fe3497fc8
mateedo <mateoshkodra9@gmail.com>
2025-08-07 00:16:04 -0700

    feat: server multiplexing, and client recieving + sending messages, also when server terminates so do all client files

--- client.cpp
+++ client.cpp
@@ -1,39 +1,74 @@
-#include <cstring>
 #include <iostream>
-#include <netinet/in.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
 
-int main()
-{
-    // creating socket
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (clientSocket == -1) {
-        std::cout << "Socket creation failed" << std::endl;
+int main() {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        std::cerr << "Socket creation error" << std::endl;
         return 1;
     }
 
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8079); // Use the same port as server
 
-    // sending connection request
-    connect(clientSocket, (struct sockaddr*)&serverAddress,
-            sizeof(serverAddress));
-
-
-    while (true) {
-        std::string message;
-
-        std::getline(std::cin, message);
-
-        send(clientSocket, message.c_str(), message.length(), 0);
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        return 1;
     }
 
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Connection Failed" << std::endl;
+        return 1;
+    }
 
-    close(clientSocket);
+    fd_set readfds;
+    int maxfd = (sock > fileno(stdin)) ? sock : fileno(stdin);
 
+    while (true) {
+        FD_ZERO(&readfds);
+        FD_SET(sock, &readfds);
+        FD_SET(fileno(stdin), &readfds);
+
+        int activity = select(maxfd + 1, &readfds, nullptr, nullptr, nullptr);
+        if (activity < 0) {
+            std::cerr << "select error" << std::endl;
+            break;
+        }
+
+        // Check for input from server
+        if (FD_ISSET(sock, &readfds)) {
+            char buffer[1024] = {0};
+            int valread = recv(sock, buffer, 1024, 0);
+            if (valread > 0) {
+                std::cout << "Message from server: " << buffer << std::endl;
+            } else if (valread == 0) {
+                std::cout << "Server disconnected. Exiting client." << std::endl;
+                break;
+            } else {
+                std::cerr << "recv error" << std::endl;
+                break;
+            }
+        }
+
+        // Check for input from user
+        if (FD_ISSET(fileno(stdin), &readfds)) {
+            std::string message;
+            if (!std::getline(std::cin, message)) {
+                // EOF or error
+                std::cout << "Input closed. Exiting client." << std::endl;
+                break;
+            }
+            if (!message.empty()) {
+                send(sock, message.c_str(), message.length(), 0);
+            }
+        }
+    }
+
+    close(sock);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -7,19 +7,36 @@
 
 int main() {
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0) {
+        std::cerr << "Failed to create socket" << std::endl;
+        return 1;
+    }
+
+    int opt = 1;
+    if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        std::cerr << "setsockopt failed" << std::endl;
+        close(serverSocket);
+        return 1;
+    }
     
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_port = htons(8079);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
+        close(serverSocket);
+        return 1;
+    }
 
-    listen(serverSocket, 5);
-
+    if (listen(serverSocket, 5) < 0) {
+        std::cerr << "Listen failed" << std::endl;
+        close(serverSocket);
+        return 1;
+    }
 
     std::vector<int> clientSockets;
-
     fd_set readfds;
 
     while (true) {
@@ -36,26 +53,47 @@ int main() {
 
         int activity = select(maxfd + 1, &readfds, nullptr, nullptr, nullptr);
         if (activity < 0) {
-            std::cout << "select error" << std::endl;
-            continue;
+            std::cerr << "select error" << std::endl;
+            break;
         }
 
         if (FD_ISSET(serverSocket, &readfds)) {
             int clientSocket = accept(serverSocket, nullptr, nullptr);
-            clientSockets.push_back(clientSocket);
-            std::cout << "New client connected" << std::endl;
+            if (clientSocket < 0) {
+                std::cerr << "accept error" << std::endl;
+            } else {
+                clientSockets.push_back(clientSocket);
+                std::cout << "New client connected" << std::endl;
+            }
         }
 
-        for (int clientSocket : clientSockets) {
+        for (auto it = clientSockets.begin(); it != clientSockets.end(); ) {
+            int clientSocket = *it;
             if (FD_ISSET(clientSocket, &readfds)) {
                 char buffer[1024] = { 0 };
                 int valread = recv(clientSocket, buffer, 1024, 0);
-
-                std::cout << "Message from client: " << buffer << std::endl;
+                if (valread > 0) {
+                    buffer[valread] = '\0';
+                    std::cout << "Message from client: " << buffer << std::endl;
+                    ++it;
+                } else if (valread == 0) {
+                    std::cout << "Client disconnected" << std::endl;
+                    close(clientSocket);
+                    it = clientSockets.erase(it);
+                } else {
+                    std::cerr << "recv error" << std::endl;
+                    close(clientSocket);
+                    it = clientSockets.erase(it);
+                }
+            } else {
+                ++it;
             }
         }
     }
 
+    for (int clientSocket : clientSockets) {
+        close(clientSocket);
+    }
     close(serverSocket);
 
     return 0;
